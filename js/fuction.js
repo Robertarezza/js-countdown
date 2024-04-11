67190f80dff060a26a5a05767db1a51d35a577d6
Robertarezza <roberta.rezza@gmail.com>
2024-04-11 18:19:42 +0200

    fix: fuction restartCaountDown

--- js/fuction.js
+++ js/fuction.js
@@ -7,28 +7,6 @@ function prinTime(s) {
 }
 
 
-/**
- * Description: start al countdown
-
- */
-function startCountdown() {
-     startCount = setInterval(function(){
-    if(seconds > 1){
-        seconds--;
-        console.log(seconds);
-    }else {
-        happyNewHear.classList.add("happy");
-        seconds = happyNewHear.innerText;
-        console.log(seconds, happyNewHear);
-        //console.log(happyNewHear.innerText);
-        clearInterval(startCount);
-        
-    }
-    prinTime(seconds)
-    
-   
-}, 1000)
-}
 
 
 /**
@@ -36,7 +14,7 @@ function startCountdown() {
  */
 function restartCountdown() {
     // Ferma il countdown attuale, se esiste
-    clearInterval(startCount);
+    //clearInterval(startCount);
 
     // Reimposta il valore del countdown
     seconds = 11;
